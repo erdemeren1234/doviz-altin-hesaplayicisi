9c661506b8201acac71dcfa25784797d72f2b310
erdemeren1234 <95621790+erdemeren1234@users.noreply.github.com>
2023-01-27 01:04:30 +0300

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@formkit/nuxt", '@nuxt/content','@vueuse/nuxt'],
+  
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const formKitTailwind = require("@formkit/themes/tailwindcss");
+
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "body-light-bg": "#ECE5C7",
+        "section-light-bg": "#C2DED1",
+      },
+    },
+  },
+  plugins: [formKitTailwind],
+};
