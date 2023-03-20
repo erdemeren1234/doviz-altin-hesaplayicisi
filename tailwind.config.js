a8873eef9ab396ae1ffbd5b4cff3053cab05180b
erdemeren1234 <erdem642372@hotmail.com>
2023-03-21 00:45:40 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
       },
       backgroundImage: {
         "section-light-bg": "linear-gradient(116deg, rgba(111,124,59,0.1) 3%, rgba(31,102,18,0.1) 40%, rgba(173,192,118,0.1) 81%)",
+        "header-bg": "linear-gradient(to left, #aabbaa80, #ffffabcc)",
+      },
+      screens: {
+        '3xl': '1600px',
       },
       
     },
